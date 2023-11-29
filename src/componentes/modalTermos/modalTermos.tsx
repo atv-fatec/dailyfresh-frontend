40a1f05a9ac003496fcae5ad228defebbbf554b4
Mariana Tamay <maritamay4@gmail.com>
2023-11-29 11:19:45 -0300

    ✨: Adição das informações do termo mais recente no modal

--- src/componentes/modalTermos/modalTermos.tsx
+++ src/componentes/modalTermos/modalTermos.tsx
@@ -1,11 +1,52 @@
 import { Modal, Button, Form } from "react-bootstrap";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IModalTermos } from "../../interface/modalTermos";
 import "./modal.css";
+import axios from "axios";
 
 export function ModalTermos(props: IModalTermos & { formData: any; setFormData: any }) {
     const { formData, setFormData } = props;
     const [validated, setValidated] = useState(false);
+    const [latestTermId, setLatestTermId] = useState<number | null>(null);
+    const [mensagem, setMensagem] = useState<string>("");
+    const [data, setData] = useState<string>("");
+    const [versao, setVersao] = useState<string>("");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                // Obter o ID mais recente
+                const idResponse = await axios.get('http://localhost:5000/term/readLatest');
+                const id = idResponse.data.latestTermId;
+
+                // Verificar se há um ID válido
+                if (id !== null && id !== undefined) {
+                    // Usar o ID para obter os dados do termo mais recente
+                    const termResponse = await axios.get(`http://localhost:5000/term/read/${id}`);
+                    const termo = termResponse.data;
+
+                    // Atualizar os estados com os dados do termo mais recente
+                    const data = new Date(termo.data.data);
+                    const adicionarZero = (numero:number) => (numero < 10 ? `0${numero}` : numero);
+                    const dia = adicionarZero(data.getDate());
+                    const mes = adicionarZero(data.getMonth() + 1);
+                    const ano = data.getFullYear();
+                    const dataFormatada = `${dia}/${mes}/${ano}`;
+
+                    setLatestTermId(id);
+                    setMensagem(termo.data.mensagem);
+                    setData(dataFormatada);
+                    setVersao(termo.data.versao);
+                } else {
+                    console.error('ID do termo mais recente inválido.');
+                }
+            } catch (error) {
+                console.error('Erro ao obter dados do termo mais recente:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     const handleAceitar = () => {
         const updatedFormData = {
@@ -51,11 +92,11 @@ export function ModalTermos(props: IModalTermos & { formData: any; setFormData: 
             >
                 <Modal.Header closeButton>
                     <Modal.Title id="contained-modal-title-vcenter" className="modal-title">
-                        Termos e Condições
+                        Termos e Condições - Versão {versao} ({data})
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>Lorem Ipsum...</p>
+                    <p>{mensagem}</p>
                     <Form noValidate validated={validated}>
                         <Form.Check
                             type="switch"
