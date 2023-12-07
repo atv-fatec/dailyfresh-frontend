d017ad578547013a3a25d8acdc6bea50e6ee95d8
taissalomao <tais.salomao@fatec.sp.gov.br>
2023-12-06 23:16:31 -0300

    edit concluido

--- src/componentes/modal/index.tsx
+++ src/componentes/modal/index.tsx
@@ -7,39 +7,36 @@ import { Botao } from "..";
 import "./modal.css"
 import axios from "axios";
 import { UserData } from "../../interface/dadosUsuario";
+import Cookies from "js-cookie";
 
 export function ModalEdit(props: IModal) {
     const [modalShow, setModalShow] = useState(false);
     const [validated, setValidated] = useState(false);
-    const userData: string | null = localStorage.getItem("usuario");
+    const userData = Cookies.get("usuario");
     const userObject = JSON.parse(userData || "{}") as UserData;
     const [formData, setFormData] = useState({
         nome: '',
-        email: '',
+        cpf: '',
         dataNascimento: '',
         telefone: '',
+        email: '',
         senha: '',
-        cpf: '',
+        obrigatorios: [] as any[], 
+        condicoes: [] as any[],     
+        meios: [] as any[], 
     });
-    const [termoData, setTermoData] = useState({
-        termos: {
-            armazenamentoDados: false,
-            pagamentoDados: false,
-            propagandas: false,
-            envioEmail: false,
-            envioSms: false,
-        },
-    });
+    const [obrigatoriosTermo, setObrigatoriosTermo] = useState<string[]>([]);
+    const [condicoesTermo, setCondicoesTermo] = useState<string[]>([]);
+    const [meiosTermo, setMeiosTermo] = useState<string[]>([]);
+    console.log(userObject)
+
     const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target;
-
         let formattedValue = value;
-
-        // Verifica se o campo é 'dataNascimento'
         if (name === 'dataNascimento') {
             formattedValue = formatMyDate(value);
         }
-
+        (userObject as any)[name] = value;
         setFormData((prevFormData) => ({
             ...prevFormData,
             [name]: formattedValue,
@@ -60,54 +57,107 @@ export function ModalEdit(props: IModal) {
         return userObject.id;
 
     };
+    
+    const handleCheckboxChangeObrigatorios = (event: any) => {
+        let { name } = event.target;
+        let  index  = name.split('-')[1]
+        index = parseInt(index)
+        const value = event.target.checked 
+        
+        setFormData({
+            ...formData,
+            obrigatorios: formData.obrigatorios.map((item: any, i: any) => {
+                if (i == index) {
+                    return value; 
+                }
+                return item; 
+            }),
+        });
+
+
+    }
+
+    const handleCheckboxChangeCondicoes = (event: any) => {
+        let { name } = event.target;
+        let  index  = name.split('-')[1]
+        index = parseInt(index)
+        const value = event.target.checked 
+        
+        setFormData({
+            ...formData,
+            condicoes: formData.condicoes.map((item: any, i: any) => {
+                if (i == index) {
+                    return value; 
+                }
+                return item; 
+            }),
+        });
+    }
+
+    const handleCheckboxChangeMeios = (event: any) => {
+        let { name } = event.target;
+        let  index  = name.split('-')[1]
+        index = parseInt(index)
+        const value = event.target.checked 
+        
+        setFormData({
+            ...formData,
+            meios: formData.meios.map((item: any, i: any) => {
+                if (i == index) {
+                    return value; 
+                }
+                return item; 
+            }),
+        });
+    }
 
     const handleSubmit = async (event: any) => {
         const form = event.currentTarget;
         event.preventDefault();
         event.stopPropagation();
-/* 
-        if (!termoData.termos.armazenamentoDados) {
-            alert("Você precisa aceitar o armazenamento de dados para prosseguir.");
-            return; // Evitar a submissão do formulário se o check não estiver marcado
-        }
- */
-        try {
-            const response = await axios.put(`http://localhost:7890/user/update/${userObject.id}`, formData);
-            const resposta = await axios.put(`http://localhost:7890/user/updateConditions/${userObject.id}`, termoData);
-        } catch (error) {
-            console.error('Erro ao enviar dados:', error);
-        } finally {
-            let userData: any = formData
-            userData.resposta = [termoData.termos]
-            localStorage.setItem("usuario", JSON.stringify(formData));
-            props.OnHide();
+
+        if (form.checkValidity() === false) {
+            console.log('Submit button clicked and validated!');
+            event.preventDefault();
+            event.stopPropagation();
+            setValidated(true);
+        } else{
+            try {
+                await axios.put(`http://localhost:2222/user/update/${userObject.id}`, formData);
+                await axios.put(`http://localhost:2222/user/updateConditions/${userObject.id}`, {obrigatorios: formData.obrigatorios, condicoes: formData.condicoes, meios: formData.meios});
+            } catch (error) {
+                console.error('Erro ao enviar dados:', error);
+            } finally {
+                let userData: any = {...userObject, ...formData}
+                /* userData.resposta = [termoData.termos] */
+                Cookies.set("usuario", JSON.stringify(userData));
+                props.OnHide();
+            }
         }
     };
 
-   /*  useEffect(() => {
-        if (props.formData && props.formData.resposta && Array.isArray(props.formData.resposta) && props.formData.resposta.length > 0) {
-            const lastResponse = props.formData.resposta[props.formData.resposta.length - 1];
-            if (lastResponse) {
-                setTermoData({
-                    termos: {
-                        armazenamentoDados: lastResponse.armazenamentoDados || false,
-                        pagamentoDados: lastResponse.pagamentoDados || false,
-                        propagandas: lastResponse.propagandas || false,
-                        envioEmail: lastResponse.envioEmail || false,
-                        envioSms: lastResponse.envioSms || false,
-                    },
-                });
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const idResponse = await axios.get('http://localhost:2222/term/readLatest');
+                const id = idResponse.data.latestTermId;
+                console.log(id)
+
+                const termResponse = await axios.get(`http://localhost:2222/term/read/${id}`);
+                const termo = termResponse.data;
+                setObrigatoriosTermo(termo.data.obrigatorios.split(','));
+                setCondicoesTermo(termo.data.condicoes.split(','));
+                setMeiosTermo(termo.data.meios.split(','));
+                setFormData({...formData, obrigatorios: termo.data.obrigatorios.split(',').map((term: any)=> false), condicoes: termo.data.condicoes.split(',').map((term: any)=> false), meios: termo.data.meios.split(',').map((term: any)=> false)})  
+
+            } catch (error) {
+                console.error('Erro ao obter dados do termo mais recente:', error);
             }
-        }
-        setFormData({
-            email: props.formData.email,
-            nome: props.formData.nome,
-            dataNascimento: props.formData.dataNascimento,
-            telefone: props.formData.telefone,
-            senha: props.formData.senha,
-            cpf: props.formData.cpf,
-        })
-    }, [props.formData]) */
+        };
+    
+        fetchData();
+        
+    }, []);
 
     return (
         <>
@@ -124,17 +174,26 @@ export function ModalEdit(props: IModal) {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-{/*                     <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
                         <Input
                             label={"Nome"}
                             type={"text"}
                             placeholder={"nome"}
                             icon={faUser}
                             cid={"idNome"}
-                            error="O campo nome é obrigatório"
                             onChange={handleInputChange}
                             name={"nome"}
-                            value={formData.nome}
+                            value={formData.nome? formData.nome : userObject.nome}
+                        />
+                        <Input
+                            label={"CPF"}
+                            type={"text"}
+                            placeholder={"000.000.000-00"}
+                            icon={faUser}
+                            cid={"idCpf"}
+                            onChange={handleInputChange}
+                            name={"cpf"}
+                            value={formData.cpf? formData.cpf : userObject.cpf}
                         />
                         <Input
                             label={"Data de nascimento"}
@@ -142,10 +201,9 @@ export function ModalEdit(props: IModal) {
                             placeholder={""}
                             icon={faCalendar}
                             cid={"idData"}
-                            error="O campo data de nascimento é obrigatório"
                             onChange={handleInputChange}
                             name={"dataNascimento"}
-                            value={formData.dataNascimento}
+                            value={formData.dataNascimento? formData.dataNascimento : userObject.dataNascimento}
                         />
                         <Input
                             label={"Telefone"}
@@ -153,97 +211,73 @@ export function ModalEdit(props: IModal) {
                             placeholder={"(12) 12345-6789"}
                             icon={faMobileScreen}
                             cid={"idTel"}
-                            error="O campo telefone é obrigatório"
                             onChange={handleInputChange}
                             name={"telefone"}
-                            value={formData.telefone}
+                            value={formData.telefone? formData.telefone : userObject.telefone}
                         />
                         <Input
                             label={"Email"}
                             type={"email"}
                             placeholder={"example@example.com"}
                             icon={faMobileScreen}
-                            cid={"idTel"}
-                            error="O campo telefone é obrigatório"
+                            cid={"idEmail"}
                             onChange={handleInputChange}
                             name={"email"}
-                            value={formData.email}
+                            value={formData.email? formData.email : userObject.email}
                         />
+                        <Input
+                            label={"Senha"}
+                            type={"senha"}
+                            placeholder={"******"}
+                            icon={faMobileScreen}
+                            cid={"idSenha"}
+                            onChange={handleInputChange}
+                            name={"senha"}
+                            value={formData.senha? formData.senha : userObject.senha}
+                        />
+                        <h4 className="cadastro-subtitle">Ao se cadastrar, você concorda com os seguintes termos e condições:</h4>
+                        {obrigatoriosTermo.map((item, index) => (
                         <Form.Check
-                            type="switch"
-                            id="aceitarDados"
-                            label="Aceitar armazenamento de dados e armazenamento de dados de pagamento"
-                            checked={termoData.termos.armazenamentoDados}
-                            onChange={() => {
-                                const updatedFormData = {
-                                    ...termoData,
-                                    termos: {
-                                        ...termoData.termos,
-                                        armazenamentoDados: !termoData.termos.armazenamentoDados,
-                                        pagamentoDados: !termoData.termos.pagamentoDados,
-                                    }
-                                };
-                                setTermoData(updatedFormData);
-                            }}
-                            className="custom-switch"
+                            onChange={(e) => handleCheckboxChangeObrigatorios(e)}
+                            required
+                            key={index}
+                            label={`${item}`}
+                            name={`obrigatorios-${index}`}
+                            type="checkbox"
+                            id={`seila-${index + 1}`}
+                            className="cadastro-check"
                         />
+                        ))}
+                        {condicoesTermo.map((item, index) => (
                         <Form.Check
-                            type="switch"
-                            id="aceitarSMS"
-                            label="Aceitar receber SMS"
-                            checked={termoData.termos.envioSms}
-                            onChange={() => {
-                                const updatedFormData = {
-                                    ...termoData,
-                                    termos: {
-                                        ...termoData.termos,
-                                        envioSms: !termoData.termos.envioSms,
-                                    }
-                                };
-                                setTermoData(updatedFormData);
-                            }}
-                            className="custom-switch"
+                            key={index}
+                            onChange={(e) => handleCheckboxChangeCondicoes(e)}
+                            label={item}
+                            name={`condicoes-${index}`}
+                            type="checkbox"
+                            id={`seila-condicoes-${index + 1}`}
+                            className="cadastro-check"
                         />
+                        ))}
+                        <hr/>
+                        <h4 className="cadastro-subtitle">Ao se cadastrar, você concorda receber notícias e propagandar por:</h4>
+                        {meiosTermo.map((item, index) => (
                         <Form.Check
-                            type="switch"
-                            id="aceitarEmail"
-                            label="Aceitar receber emails"
-                            checked={termoData.termos.envioEmail}
-                            onChange={() => {
-                                const updatedFormData = {
-                                    ...termoData,
-                                    termos: {
-                                        ...termoData.termos,
-                                        envioEmail: !termoData.termos.envioEmail,
-                                    }
-                                };
-                                setTermoData(updatedFormData);
-                            }}
-                            className="custom-switch"
+                             key={index}
+                             onChange={(e) => handleCheckboxChangeMeios(e)}
+                            label={item}
+                            name={`meios-${index}`}
+                            type="checkbox"
+                            id={`seila-meios-${index + 1}`}
+                            className="cadastro-check"
                         />
-                        <Form.Check
-                            type="switch"
-                            id="aceitarPropaganda"
-                            label="Aceitar receber propaganda"
-                            checked={termoData.termos.propagandas}
-                            onChange={() => {
-                                const updatedFormData = {
-                                    ...termoData,
-                                    termos: {
-                                        ...termoData.termos,
-                                        propagandas: !termoData.termos.propagandas,
-                                    }
-                                };
-                                setTermoData(updatedFormData);
-                            }}
-                            className="custom-switch"
-                        />
+                        ))}
                         <Botao
                             label="Salvar"
                             id="signup-btn"
                             type="submit"
                         />
-                    </Form> */}
+                    </Form>
                 </Modal.Body>
 
             </Modal>
